bbe176767501552c188e2b01a538d21c52bccfa7
MartinFrank77 <martin.frank@irix.ch>
2025-10-20 13:45:22 +0200

    initial commit

---
+++ src/main/java/com/github/martinfrank/nethackagent/nethackagent/NetHackAgentApplication.java
@@ -0,0 +1,13 @@
+package com.github.martinfrank.nethackagent.nethackagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetHackAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetHackAgentApplication.class, args);
+	}
+
+}
